f36fd3a3ed7a513acf68b1b9a9f9b76e8b79ce39
sean-rohan <32681510+sean-rohan@users.noreply.github.com>
2018-09-25 13:26:23 -0700

    Modified tag_residuals_direct
    Updated to return models along with residual data frame.

--- R/tag_residuals_direct.R
+++ R/tag_residuals_direct.R
@@ -8,6 +8,7 @@
 #' @param surface.col A character vector with the name of the column containing surface light measurements.
 #' @param depth.col A character vector with the name of the column containing depths.
 #' @param depth.bins Depth bins for which linear regression should be conducted.
+#' @param ... Additional arguments
 #'
 #' @author S.K. Rohan \email{skrohan@@uw.edu}
 
@@ -15,23 +16,33 @@ tag_residuals_direct <- function(x, formula = log10(trans_llight) ~ log10(surf_t
   names(x)[names(x) == water.col] <- "trans_llight"
   names(x)[names(x) == surface.col] <- "surf_trans_llight"
   names(x)[names(x) == depth.col] <- "cdepth"
+  lout <- list()
+
+  if(mean(c(depth.bins) %in% x$cdepth)) {
+    stop(paste0("tag_residuals_direct: Cannot calculate residuals. Some depth.bins not found in ", depth.col))
+  }
+
   for(i in 1:length(depth.bins)) {
     x_sub <- subset(x, cdepth == depth.bins[i])
 
     # Linear regression
-    DIRECT_LM <- lm(formula, data = x_sub)
+    DIRECT_LM <- lm(formula, data = x_sub, ...)
 
     # Append lin. reg. residuals to subset data frame
     x_sub$direct_residual <- residuals(DIRECT_LM)
     if(depth.bins[1] == depth.bins[i]) {
       output.df <- x_sub
     } else {
-      output.df <- plyr::rbind.fill(output.df, x_sub)
+      output.df <- plyr::rbind.fill(output.df, x_sub, ...)
     }
+   lout[[i]] <- DIRECT_LM
+   names(lout)[i] <- paste0("lm_dbin", depth.bins[i])
 
     names(x)[names(x) == "trans_llight"] <- water.col
     names(x)[names(x) == "surf_trans_llight"] <- surface.col
     names(x)[names(x) == "cdepth"] <- depth.col
   }
-  return(output.df)
+
+  lout$resid_df <- output.df
+  return(lout)
 }
