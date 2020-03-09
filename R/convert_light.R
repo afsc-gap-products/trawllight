ae24d555d03e9e20ec5ceb6821c778bd14c8836a
sean-rohan <32681510+sean-rohan@users.noreply.github.com>
2020-03-08 19:23:35 -0700

    Update convert_light

--- R/convert_light.R
+++ R/convert_light.R
@@ -9,6 +9,7 @@
 #' @references Kotwicki, S., Robertis, A., von Szalay, P., and Towler, R. 2009. The effect of light intensity on the availability of walleye pollock (Theragra chalcogramma) to bottom trawl and acoustic surveys. Can. J. Fish. Aquat. Sci. 66(6): 983–994. doi:10.1139/f09-055.
 
 convert_light <- function(x, convert.method = "kotwicki") {
+  print(convert.method)
   convert.method <- tolower(convert.method)
   if(convert.method == "kotwicki") {
     y <- 1*10^-8 * exp(0.1322*x)
